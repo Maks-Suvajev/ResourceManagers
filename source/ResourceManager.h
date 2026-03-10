176e792bf8eec0cd72479c15f5a5ae071b090085
DESKTOP-58047GN\Maks- <maksimilian.suvajev@gmail.com>
2026-03-10 16:16:05 +0100

    New resourceManager generic interface created for UI integration

---
+++ source/ResourceManager.h
@@ -0,0 +1,82 @@
+#ifndef RESOURCE_MANAGER_H
+#define RESOURCE_MANAGER_H
+
+// STL
+#include <filesystem>
+#include <unordered_map>
+#include <string>
+#include <memory>
+
+#include "AssetRegistry.h"
+
+template<typename T>
+class ResourceManager
+{
+    public:
+        ResourceManager(AssetRegistry* assetRegistry, std::vector<std::string> supportedFileTypes);
+        virtual ~ResourceManager() = default;
+
+        void deleteElement(std::string key);
+        virtual void refreshElements() = 0;
+
+        std::vector<std::string> getKeys();
+        const std::unordered_map<std::string, std::unique_ptr<T>>& getMap();
+
+        std::string getCurrentActiveDirectory();
+        void setCurrentActiveDirectory(std::string directory);
+
+    protected:
+        AssetRegistry*                                      m_assetRegistry;
+        std::unordered_map<std::string, std::unique_ptr<T>> m_elements;
+        std::filesystem::path                               m_activeDirectory;
+        std::vector<std::string>                            m_supportedFileTypes;
+};
+
+template<typename T>
+void ResourceManager<T>::deleteElement(std::string key)
+{
+    m_elements.erase(key);
+}
+
+template<typename T>
+ResourceManager<T>::ResourceManager(AssetRegistry* assetRegistry, std::vector<std::string> supportedFileTypes)
+    : m_assetRegistry(assetRegistry),
+      m_supportedFileTypes(supportedFileTypes)
+{
+    m_activeDirectory = m_assetRegistry->getDefaultAssetPath<T>();
+}
+
+template<typename T>
+std::vector<std::string> ResourceManager<T>::getKeys()
+{
+    std::vector<std::string> keys;
+
+    for (const auto& [key, element] : m_elements)
+    {
+        keys.push_back(key);
+    }
+
+    return keys;
+}
+
+template<typename T>
+const std::unordered_map<std::string, std::unique_ptr<T>>& ResourceManager<T>::getMap()
+{
+    return m_elements;
+}
+
+template<typename T>
+std::string ResourceManager<T>::getCurrentActiveDirectory()
+{
+    return m_activeDirectory.string();
+}
+
+template<typename T>
+void ResourceManager<T>::setCurrentActiveDirectory(std::string directory)
+{
+    m_activeDirectory = std::filesystem::path(directory);
+}
+
+
+
+#endif
